0381bd1c74602e8615b242c9f008a81fbd34d9e9
Lazar Bodor <lazar.bodor@ga.gov.au>
2016-01-06 13:51:11 +1100

    Docflex is now in doc/xsd-generated

--- docflex-xml-1.9.5/integrations/XMLSpy/linux/xmlspy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#----------------------------------------------------------------------
-# You can use this shell script simply to test/run XMLSpy under Linux
-#----------------------------------------------------------------------
-wine 'C:\Program Files\Altova\XMLSpy2014\XMLSpy.exe'
--- docflex-xml-1.9.5/integrations/ant/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-ANT_HOME=/usr/local/apache-ant-1.8.3
-JAVA_HOME=/usr/java/jdk1.7.0
-
-${ANT_HOME}/bin/ant XSDDoc
-
-sleep 10
