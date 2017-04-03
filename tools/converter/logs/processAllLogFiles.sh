01872d5c8188726d9f06b1746304586da1130a0e
lbodor <lazar.bodor@ga.gov.au>
2017-04-03 10:34:53 +1000

    Tidy up

--- tools/converter/logs/processAllLogFiles.sh
+++ tools/converter/logs/processAllLogFiles.sh
@@ -1,24 +1,14 @@
 #!/usr/bin/env bash
 
-#for file in yar3_20140709.log
-for file in ./*.log
-do
-#    echo "file = " ${file}
-    
-    name=${file##*/}
-#    echo "name = " ${name}
-
-    base=${name%.log}
-#    echo "base = " ${base}
-    
-    xmlFileName=${base%.xml}
-#    echo "xmlFileName = " ${xmlFileName}.xml
-    
-    # check if the file exists    
-    if [ ! -f xmlOutput/${xmlFileName}.xml ]; 
-        then 
-#            printf "\nprocesssing input: ${name} and output ${xmlFileName}.xml\n"    
+if ls ./*.log 1> /dev/null 2>&1; then
+    for file in ./*.log; do
+        echo $file
+        name=${file##*/}
+        base=${name%.log}
+        xmlFileName=${base%.xml}
+        if [ ! -f xmlOutput/${xmlFileName}.xml ]; then 
+            printf "\nprocesssing input: ${name} and output ${xmlFileName}.xml\n"    
             python ../log2xml.py -l ${name} -g ../logging.conf -x xmlOutput/${xmlFileName}.xml -v
-    fi
-
-done
+        fi
+    done
+fi
