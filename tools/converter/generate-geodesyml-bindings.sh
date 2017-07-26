cab574f64ddeefb169021ff9130b2c5aa258e2c5
lbodor <lazar.bodor@ga.gov.au>
2017-07-26 13:10:50 +1000

    Deprecate tools/converter
    See https://github.com/GeoscienceAustralia/GeodesyMLConverter.

--- tools/converter/generate-geodesyml-bindings.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-PYTHON_PREFIX=${HOME}/.local
-PYXBGEN=${PYTHON_PREFIX}/bin/pyxbgen
-
-${PYXBGEN} -u ./modified-schemas/geodesyML.xsd -m eGeodesy --archive-path "${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/common/raw/:"${PYTHON_PREFIX}"/lib/python2.7/site-packages/pyxb/bundles/opengis/raw/:.:+
