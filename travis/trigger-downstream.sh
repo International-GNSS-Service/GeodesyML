9458d4a6fd73903f0df3472896d42725c219564e
lbodor <lazar.bodor@ga.gov.au>
2017-08-08 14:00:28 +1000

    Update downstream dependency

--- travis/trigger-downstream.sh
+++ travis/trigger-downstream.sh
@@ -3,6 +3,6 @@
 # shellcheck source=/dev/null
 . <(curl -s https://raw.githubusercontent.com/GeoscienceAustralia/trigger-travis/7c1b0f1d5abb02381805d8a4ccc467b1db5e6422/trigger-travis.sh)
 
-declare -a downstream=("ogc-schemas")
+declare -a downstream=("GeodesyML-Java-Bindings")
 trigger-travis "${downstream[@]}"
 
