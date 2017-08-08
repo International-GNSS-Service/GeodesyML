b28ad28aff8fa0c9a6918e3a3f55c71065cd899f
lbodor <lazar.bodor@ga.gov.au>
2017-08-08 13:27:08 +1000

    Remove http_proxy from schematronValidate.sh
    It is not needed.

--- tools/schematron/schematronValidate.sh
+++ tools/schematron/schematronValidate.sh
@@ -26,13 +26,6 @@ else
 	outfile=$infile-validate.xml
 fi
 
-# shellcheck disable=SC2154
-if [ "$http_proxy" ]; then
-    proxyHost=$(echo "$http_proxy" | sed 's/http[s]*:\/\///' | sed 's/:.*//')
-    proxyPort=$(echo "$http_proxy" | sed 's/http[s]*:\/\///' | sed 's/.*://')
-    javaFlags="-Dhttp.proxyHost=$proxyHost -Dhttp.proxyPort=$proxyPort -Dhttps.proxyHost=$proxyHost -Dhttps.proxyPort=$proxyPort"
-fi
-
 if [ -n "$JAVA_HOME" ]; then
     javaCmd="${JAVA_HOME}/bin/java"
 else
@@ -40,17 +33,15 @@ else
 fi
 
 # Build the XSLT for the schematron
-${javaCmd} "$javaFlags" \
-		-jar "$saxonJar" \
-		-s:"$schematronScript" \
-		-xsl:"$schematronHome"/iso_svrl_for_xslt2_with_diagnostics.xsl \
-		-o:"$schematronScript.xsl"
+${javaCmd} -jar "$saxonJar" \
+    -s:"$schematronScript" \
+    -xsl:"$schematronHome"/iso_svrl_for_xslt2_with_diagnostics.xsl \
+    -o:"$schematronScript.xsl"
 
 # Validate the input using the Schematron XSLT
-${javaCmd} "$javaFlags" \
-		-jar "$saxonJar" \
-		-s:"$infile" -xsl:"$schematronScript.xsl" \
-		-o:"$outfile"
+${javaCmd} -jar "$saxonJar" \
+    -s:"$infile" -xsl:"$schematronScript.xsl" \
+    -o:"$outfile"
 
 # shellcheck disable=SC2034
 failures=$(grep -i "failed-assert" "$outfile")
