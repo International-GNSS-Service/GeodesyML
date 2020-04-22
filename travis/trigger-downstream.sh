ed961fa54e6c0df3fcd28af065735c3c697db387
lbodor <lazar.bodor@ga.gov.au>
2020-04-22 14:17:35 +1000

    Adjust trigger-downstream.sh target branch

--- travis/trigger-downstream.sh
+++ travis/trigger-downstream.sh
@@ -4,5 +4,5 @@
 . <(curl -s https://raw.githubusercontent.com/GeoscienceAustralia/trigger-travis/c1e157f8a648cbf90a18a75e29408af3bf9a3820/trigger-travis.sh)
 
 declare -a downstream=("GeodesyML-Java-Bindings")
-trigger-travis -b next "${downstream[@]}"
+trigger-travis -b geodesyml-0.4 "${downstream[@]}"
 
